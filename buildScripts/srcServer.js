4dbd5780f1cf1690fdd9740fa92115dbbb519ab9
Jorawar Ubhi <jorubhi@publicisgroupe.net>
2018-09-05 14:44:17 +0530

    nodejs server + mongodb

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -12,10 +12,37 @@ import open from 'open';
 const port = 4000;
 const app = express();
 
+var MongoClient = require('mongodb').MongoClient
+    , assert = require('assert');
+
+// Connection URL to the DB
+var url = 'mongodb://localhost:27017/AngularApi';
+
+
+
+MongoClient.connect(url, function (err, client) {
+    if (err) throw err;
+
+    var db = client.db('AngularApi'); //
+
+    var cursor = db.collection('users').find();
+
+    cursor.forEach(function (item) {
+
+        console.log(item)
+
+    });
+
+});
+
+
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'))
 
 })
+
+
 
 app.listen(port, function (err) {
     if (err) {
